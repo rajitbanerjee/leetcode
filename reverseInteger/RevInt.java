73e37b19e53b27e02ac12dcc08f03a57b08c2295
rajitbanerjee <rajit.banerjee@ucdconnect.ie>
2019-06-11 21:45:44 +0530

    Added some solutions in java

---
+++ reverseInteger/RevInt.java
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/reverse-integer/ Given a 32-bit signed integer,
+ * reverse digits of an integer. Assume we are dealing with an environment which
+ * could only store integers within the 32-bit signed integer range: [−2^31,
+ * 2^31 − 1]. For the purpose of this problem, assume that your function returns
+ * 0 when the reversed integer overflows.
+ */
+class RevInt {
+    public int reverse(int x) {
+        long copy = x, rev = 0;
+
+        // reverse the number digit-by-digit
+        while (copy != 0) {
+            rev = rev * 10 + copy % 10;
+            copy = copy / 10;
+        }
+
+        // overflow case
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE)
+            return 0;
+        else
+            return (int) (rev); // convert long to int and return
+
+    }
+}
