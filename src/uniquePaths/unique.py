1f2c634849f7d2ee1d6ae346d050d24822d9b917
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2020-10-01 21:29:16 +0100

    Unique Paths

--- src/uniquePaths/unique.py
+++ src/uniquePaths/unique.py
@@ -1,12 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         # store results of subproblems
-        dp = [1 for _ in range(n)]
-        for _ in range(m - 1):
-            for j in range(1, n):
-                dp[j] += dp[j - 1]
+        dp = [[0 for _ in range(n)] for _ in range(m)]
 
-        return dp[n - 1]
+        for i in range(m):
+            for j in range(n):
+                if 0 in [i, j]:
+                    dp[i][j] = 1
+                else:
+                    # any given grid index can be reached either from the top or left
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+
+        return dp[m-1][n-1]
 
 
 if __name__ == '__main__':
